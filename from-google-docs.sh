8281bbd12a2d5f82174eed582557b32a13f96d0a
Alexandre Magno <alexandre.mbm@gmail.com>
2014-09-25 06:28:17 -0300

    Initial commit

---
+++ from-google-docs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright (c) 2014 Alexandre Magno <alexandre.mbm@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+cp "sisloc-planilha - data.tsv" sisloc-fdocs.csv
+sed -i "s/\t\t*/;/g" sisloc-fdocs.csv
+
+GREP=$(grep --color=always -n -E "([^;]+[^yY;][^eE][^sS]$)|([nN][oO]$)" sisloc-fdocs.csv)
+
+# yes no changed suspect
+
+if [ $? -ne 0 ] || [ -n "$GREP" ]; then
+  (
+    echo "Veja as PENDÊNCIAS abaixo e conserte no Google Docs:"
+    echo
+    echo "$GREP"
+  ) | less -R
+  exit 1
+fi
+
+sed -e "s/;[^;]*$//g" "sisloc-planilha - data.tsv" > sisloc.csv 
+
+echo "Seu arquivo final é sisloc.csv"
